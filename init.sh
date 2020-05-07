8280c9f084a46ddd343a678660165f8455d53d7f
Natan Barbosa <natanbarbosa15@gmail.com>
2020-05-07 17:35:14 -0300

    Log line prefix for postgres to show user IP.

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Start Fail2Ban service and postgres with logging enabled.
-service fail2ban start && docker-entrypoint.sh -c logging_collector=on -c log_directory=/var/log/postgresql -c log_filename=postgresql.log -c log_statement=all
+service fail2ban start && docker-entrypoint.sh -c logging_collector=on -c log_directory=/var/log/postgresql -c log_filename=postgresql.log -c log_statement=all -c log_line_prefix='%m [%p] IP=%h '
