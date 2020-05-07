a5d3033d608cfef49357cc991e100efdb3b47a76
Natan Barbosa <natanbarbosa15@gmail.com>
2020-05-07 20:03:34 -0300

    Fix init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Start Fail2Ban service and postgres with logging enabled.
-service fail2ban start && docker-entrypoint.sh -c logging_collector=on -c log_directory=/var/log/postgresql -c log_filename=postgresql.log -c log_statement=all -c log_line_prefix='%m [%p] IP=%h '
+# Start Fail2Ban service, restart and reload Fail2Ban client, and start postgres with logging enabled.
+service fail2ban start && fail2ban-client restart && fail2ban-client reload \
+    && docker-entrypoint.sh -c logging_collector=on -c log_directory=/var/log/postgresql -c log_filename=postgresql.log -c log_statement=all -c log_line_prefix='%m [%p] IP=%h '
